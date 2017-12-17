a05a9f6b712c7d582dbe90158c8d6f0f2f7cf489
BrechtDecuyper <brechtdecuyper@hotmail.com>
2017-12-17 15:08:54 +0100

    Some changes for part 2

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "***************************************************"
+echo "*           Starting Destination Server           *"
+echo "***************************************************"
+java -jar /usr/local/DestinationService/@project.build.finalName@.jar
---
+++ target/dockerfile/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "***************************************************"
+echo "*           Starting Destination Server           *"
+echo "***************************************************"
+java -jar /usr/local/DestinationService/DestinationService-0.0.1-SNAPSHOT.jar
